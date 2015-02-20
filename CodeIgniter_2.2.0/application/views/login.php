577dd01993710c76c0b9bb29a86ab6342bec8a4f
santosdanilo <dan-desenvolvimento@hotmail.com>
2015-02-20 15:42:15 -0200

    Testando header e login

---
+++ CodeIgniter_2.2.0/application/views/login.php
@@ -0,0 +1,36 @@
+  <body>
+    <div id="header">
+      <div id="logo">
+        <img src="./images/logo.png"/>
+      </div>
+      <div id="text-logo">
+        <div id="title-logo">
+          <h1>Processo Seletivo</h1>
+        </div>
+        <div id="subtitle-logo">
+          <span class="subtitle-url"><h5><a href="http://www.cjr.org.br/site/">http://www.cjr.org.br/site/  </a></h5></span>
+        </div>
+      </div>
+    </div>
+    <div id="body">
+    </div>
+
+    <div id="login-field">
+      <div id="login-user">
+        <img src="./images/icon_user.png" class="icon"/>
+        <input type="text" name="login" placeholder="USUÁRIO" value="">
+      </div>
+      <div id="login-password">
+        <img src="./images/icon_password.png" class="icon"/>
+        <input type="password" name="password" placeholder="SENHA" value="">
+        <a href="#">Esqueci minha senha</a>
+        <div id="icon-cadastro">
+
+        </div>
+      </div>
+
+    </div>
+
+
+  </body>
+</html>
