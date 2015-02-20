3c20c7a439ec1f064153837dac1ae52b7b2bad2f
santosdanilo <dan-desenvolvimento@hotmail.com>
2015-02-20 15:36:56 -0200

    Testando header

---
+++ CodeIgniter_2.2.0/application/views/_inc/header.php
@@ -0,0 +1,12 @@
+<?php
+<html>
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<head>
+  <title>Login</title>
+  <script src="./media/javascript/sweetalert/lib/sweet-alert.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="./media/javascript/sweetalert/lib/sweet-alert.css">
+  <link rel="stylesheet" type="text/css" href="./media/css/layout.css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,600italic,400italic,300' rel='stylesheet' type='text/css'>
+</head>
+ ?>
