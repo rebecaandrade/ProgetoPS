eaa3972cf13dee2c731dffd5eec686de133e9b9f
Vitor Pontes <vitorpontes@cjr.org.br>
2015-02-21 18:39:13 -0200

    pasta na views 'access' criada

---
+++ CodeIgniter_2.2.0/application/views/access/login.php
@@ -0,0 +1,21 @@
+<?php echo $this->load->view('_inc/header_big')?>
+
+
+<?php echo form_open('access/sign_in') ?>
+	<div id="login-field">
+		<div id="login-user">
+	    	<img src="<?php echo base_url();?>assets/images/icon_user.png" class="icon"/>
+	    	<input type="text" name="login" placeholder="USUÁRIO" value="">
+	    </div>
+	    <div id="login-password">
+	    	<img src="<?php echo base_url();?>assets/images/icon_password.png" class="icon"/>
+	    	<input type="password" name="password" placeholder="SENHA" value="">
+	    	<input type="submit" value="Entrar">
+	    	<a href="<?php echo base_url(); ?>index.php/access/password_recovery">Esqueci minha senha</a>
+	    	<div id="icon-cadastro"> </div>
+	    </div>
+	</div>	
+<?php echo form_close() ?>
+
+
+<?php echo $this->load->view('_inc/footer')?>
