0fffd60b581fed9a8998e4520d6536060c523a83
igorsodre <igorcuei@yahoo.com.br>
2015-02-23 00:28:15 -0300

    consertos no listar candidatos

--- CodeIgniter_2.2.0/application/views/admin/admin_page.php
+++ CodeIgniter_2.2.0/application/views/admin/admin_page.php
@@ -1,32 +1,32 @@
 <?php echo $this->load->view('_inc/header_big')?> 
 <div id="subtitle-logo-small">
-          <span class="subtitle-url"><h4>Administrador</h4></span>
-        </div>
-      </div>
-    </div>
+		  <span class="subtitle-url"><h4>Administrador</h4></span>
+		</div>
+	  </div>
+	</div>
 
 <div id="body-admin">
   <div id="body-admin-info">
-    <h5>Informações Gerais</h5>
+	<h5>Informações Gerais</h5>
   </div>
   <div id="body-admin-nav">
-    <div class="admin-nav">
-      <img src="./images/person_admin.png" alt="" />
-      <p>
-        Candidatos
-      </p>
-    </div>
-    <div class="admin-nav">
-      <img src="./images/list_admin.png" alt="" />
-      <p>
-        Administradores
-      </p>
-    </div>
-    <div class="admin-nav">
-      <img src="./images/ps_admin.png" alt="" />
-      <p>
-        Processo Seletivo
-      </p>
-    </div>
+	<div class="admin-nav">
+	  <a href="<?php echo base_url();?>/index.php/usuario/list_users"><img src="<?php echo base_url();?>assets/images/person_admin.png" alt="" /></a>
+	  <p>
+		Candidatos
+	  </p>
+	</div>
+	<div class="admin-nav">
+	  <img src="<?php echo base_url();?>assets/images/list_admin.png" alt="" />
+	  <p>
+		Administradores
+	  </p>
+	</div>
+	<div class="admin-nav">
+	  <img src="<?php echo base_url();?>assets/images/ps-admin.png" alt="" />
+	  <p>
+		Processo Seletivo
+	  </p>
+	</div>
   </div>
 </div>
