2312a32f405e47a737edf20bf93c37c5d1666d5f
danilo_santos <danilosantos@cjr.org.br>
2015-02-24 20:36:11 -0300

    Criando página de visualizar informações user

--- CodeIgniter_2.2.0/application/views/admin/user_info.php
+++ CodeIgniter_2.2.0/application/views/admin/user_info.php
@@ -4,15 +4,55 @@
 		</div>
 	  </div>
 	</div>
+	<!--
 	<div id="dados" class="lista">
 		<br><br>
 		<p>Nome : <?php echo $user->nome; ?> <br /></p>
-		<p>Usuario : <?php echo $user->usuario; ?> <br /></p>
 		<p>E-mail : <?php echo $user->email;?> <br /></p>
 		<p>Curso : <?php echo $user->curso; ?> <br /></p>
 		<p>Semestre : <?php echo $user->semestre;?> <br /></p>
 		<p>Telefone : <?php echo $user->telefone; ?> <br /></p>
-		<p>Número de PS(s) que participou : <?php echo $user->num_de_ps; ?> <br /></p>
+	 </div>
+-->
+		<div id="user-info">
+			<div class="profile-photo">
+				<img src="<?php echo base_url();?>assets/images/photo_profile.png" alt="" />
+				<img src="<?php echo base_url();?>assets/images/foto_usuario.png" alt="" />
+				<p><?php echo $user->nome; ?> <br /</p>
+			</div>
+			<div id="profile-data">
+			<p>
+				Curso:
+				</p>
+			<p><?php echo $user->curso; ?></p>
+			<p>
+				<br />
+				Semestre:
+			</p>
+			<p><?php echo $user->semestre;?></p>
+			<p>
+				<br />
+				Email:
+			</p>
+			<p><?php echo $user->email;?> </p>
+			<p>
+				<br />
+				Telefone:
+			</p>
+			<p><?php echo $user->telefone; ?></p>
+			<p>Usuario : <?php echo $user->usuario; ?> <br /></p>
+			<p>Número de PS(s) que participou : <?php echo $user->num_de_ps; ?> <br /></p>
+			</div>
+		</div>
 
-	   </div>
+		<div id="user-activity">
+			<div id="user-activity-nav-top">
+				<img src="<?php echo base_url();?>assets/images/status_profile_out.png" alt="" />
+			</div>
+			<div id="user-activity-nav-body">
+		</div>
+
+		<div id="user-interview">
+
+		</div>
 <?php echo $this->load->view('_inc/footer') ?>
