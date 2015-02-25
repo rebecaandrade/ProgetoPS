1724ccbeaa90ddfa445ce5c285de04f3408bf2a9
danilo_santos <danilosantos@cjr.org.br>
2015-02-25 16:27:21 -0300

    Pagina de Feedback /2

--- CodeIgniter_2.2.0/application/views/admin/admin_feedback.php
+++ CodeIgniter_2.2.0/application/views/admin/admin_feedback.php
@@ -1,3 +1,5 @@
+<?php echo $this->load->view('_inc/header_small')?>
+
 <div id="subtitle-logo-small">
   <span class="subtitle-url"><h4>Preencher FeedBack</h4></span>
 </div>
@@ -5,5 +7,34 @@
 </div>
 
 <div id="body-admin-feedback">
-  
+  <div id="admin-feeback-top">
+    <img src="<?php echo base_url();?>assets/images/photo_feedback.png" alt="" />
+    <h5>Rebeca</h5>
+  </div>
+  <div id="admin-feeback-body">
+    <div id="feedback-body-text">
+      <p>
+        Fazer uma homenagem ao Billy
+      </p>
+    </div>
+    <div id="feedback-body-interviewers">
+      <div id="interviwers-button">
+        <img src="<?php echo base_url();?>assets/images/profile_post.png" alt="" />
+        <p>
+          Entrevistadores
+        </p>
+      </div>
+      <div id="interviwers-list">
+        <p>
+          fgfdg
+        </p>
+      </div>
+    </div>
+  </div>
+  <div id="admin-feeback-footer">
+    <img src="<?php echo base_url();?>assets/images/button_light_cancel.png" alt="" />
+    <img src="<?php echo base_url();?>assets/images/button_light_accept.png" alt="" />
+  </div>
 </div>
+
+<?php echo $this->load->view('_inc/footer')?>
