fb2c6b8ea277cc0f703c27df70a2637848e29bbb
rebecaandrade <rebecaandrade@cjr.org.br>
2015-03-05 20:30:42 -0300

    2.2
    pequenas mudanças

--- CodeIgniter_2.2.0/application/controllers/ps.php
+++ CodeIgniter_2.2.0/application/controllers/ps.php
@@ -10,13 +10,13 @@ class PS extends CI_Controller {
 /// Carregando páginas
     public function lista(){
     	$this->load->model('PS_model');
-    	$dados['tb_PS'] = $this->PS_model->searchPS();	
+    	$dados['tb_PS'] = $this->PS_model->search_ps();	
     	$this->load->view('admin/user_list', $dados);
     }
 /// Serviços
     public function cadastrar(){
         $this->load->model('PS_model');
-        if($this->PS_model->getStatus()){
+        if($this->PS_model->current_ps()){
         	$dados = array(
         		'nome' => $this->input->post('nome'),
         		'data_abertura' => $this->input->post('data_abertura'),	
@@ -34,7 +34,7 @@ class PS extends CI_Controller {
     public function excluir($id){
     	$this->load->model('PS_model');
     	try{
-        	$this->PS_model->closePS($id);
+        	$this->PS_model->close_ps($id);
         } catch(Exception $e){
         	$this->session->set_userdata('mensagem', $e->getMessage());
         }
@@ -44,7 +44,7 @@ class PS extends CI_Controller {
     public function selecionar($id){
     	$this->load->model('PS_model');
     	try{
-    		$this->PS_model->showPS($id);
+    		$this->PS_model->show_ps($id);
     	} catch(Exception $e){
     		$this->session->set_userdata('mensagem', $e->getMessage());
     	}
--- CodeIgniter_2.2.0/application/models/ps_model.php
+++ CodeIgniter_2.2.0/application/models/ps_model.php
@@ -2,13 +2,8 @@
 
 class PS_Model extends CI_Model{
 
-	public function getStatus(){
-		$this->db->where('status_ps',1);
-		return $this->db->get('tb_ps')->result();	
-	}
-
 	///Abrir PS 
-	public function newPS($id, $nome, $data_abetura, $data_dinamica, $data_apresentacao, $primeiro_horario_dinamica, 
+	public function new_ps($id, $nome, $data_abetura, $data_dinamica, $data_apresentacao, $primeiro_horario_dinamica, 
 						  $segundo_horario_dinamica, $primeiro_horario_apresentacao, $segundo_horario_apresentacao){
 
 	$this->db->insert('id',$id);
@@ -23,7 +18,7 @@ class PS_Model extends CI_Model{
 
 	}
 	///Fechar PS
-	public function closePS($id){
+	public function close_ps($id){
 		$this->db->where('id',$id);
 	    return $this->db->delete('tb_PS');
 	}
@@ -33,12 +28,12 @@ class PS_Model extends CI_Model{
 		return $this->db->get('tb_ps')->row()->id;
 	}
 	///Listar
-	public function searchPS(){
+	public function search_ps(){
 		return $this->db->get('tb_PS')->result();
 
 	}
 	///Selecionar (visualizar) PS
-	public function showPS($id){
+	public function show_ps($id){
 		$this->db->where('id',$id);
 		return $this->db->get('tb_PS')->result();
 	}
