f0f0bd2b789bceb8f2361d3649b71910cadc757a
danilosantoscjr <danilosantos@cjr.org.br>
2015-03-06 12:44:38 -0300

    Dando merge /9

--- CodeIgniter_2.2.0/application/views/user/user_feedback.php
+++ CodeIgniter_2.2.0/application/views/user/user_feedback.php
@@ -1,5 +1,4 @@
 <?php echo $this->load->view('_inc/header_thin') ?>
-
 	  <div class="header-text-thin-square-2 header-text-uppercase">
 		<p>feedback</p>
 	  </div>
@@ -8,28 +7,31 @@
   <div id="content" class="content-thin">
 	<div id="feedback">
 	  <div id="feedback-text">
+		<!--
 		<div id="feedback-text-message">
 		  <p>
-		   <?php echo $feeds->feedback ?>
+		   <?php// echo $feeds->feedback ?>
 		  </p>
 		</div>
 		<div id="feedback-text-footer">
 		  d
 		</div>
-	  </div>
-
-	  <div id="feedback-nav">
+		-->
+		</div>
+		<div id="feedback-nav">
+			<!--
 		<div id="feedback-nav-top">
 			<p>
 			  edição
 		 	</p>
 		</div>
 		<div id="feeback-nav-body">
-		 	<?php foreach($semestres as $date => $semestre){?>
-				<a href="<?php echo base_url()?>index.php/feedback/load_feedback/<?php echo $dates[$date] ?>"><?php echo $semestre ?></a><br>
-			<?php }?>
+		 	<?php// foreach($semestres as $date => $semestre){?>
+				<a href="<?php// echo base_url()?>index.php/feedback/load_feedback/<?php// echo $dates[$date] ?>"><?php// echo $semestre ?></a><br>
+			<?php// }?>
 		</div>
-	</div>
+		-->
+		</div>
 	</div>
   </div>
 
