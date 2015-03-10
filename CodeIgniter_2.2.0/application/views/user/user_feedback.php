67d77470e6cd9ac60ead8229307c9c347f1780c4
igorsodre <igorcuei@yahoo.com.br>
2015-03-09 21:02:09 -0300

    imagem de perfil

--- CodeIgniter_2.2.0/application/views/user/user_feedback.php
+++ CodeIgniter_2.2.0/application/views/user/user_feedback.php
@@ -14,27 +14,16 @@
 		 	</p>
 		</div>
 		<div id="feedback-nav-body">
+		 	<?php if(isset($semestres)){?>
 		 	<?php foreach($semestres as $date => $semestre){?>
 				<a href="<?php echo base_url()?>index.php/feedback/load_feedback/<?php echo $dates[$date] ?>">
 				<p>
 				<?php echo $semestre ?>
 				</p></a><br>
-			<?php }?>
+			<?php }}?>
 		</div>
 		</div>
 	  <div id="feedback-text">
-<<<<<<< HEAD
-		<div id="feedback-text-message">
-		  <p>
-		   <?php echo $feeds->feedback ?>
-		  </p>
-		</div>
-		<div id="feedback-text-footer">
-			<p>
-				Deseja marcar um Feedback presencial?
-			</p>
-=======
-
 		<div id="feedback-text-message">
 		 	<p>
 		  		<?php if(sizeof($feeds) > 0){?>
@@ -44,21 +33,6 @@
 		</div>
 		<div id="feedback-text-footer">
 		  d
-		</div>
-		</div>
-		<div id="feedback-nav">
-		<div id="feedback-nav-top">
-			<p>
-			  edição
-		 	</p>
-		</div>
-		<div id="feeback-nav-body">
-
-			<?php if(isset($semestres)){?>
-		 	<?php foreach($semestres as $date => $semestre){?>
-				<a href="<?php echo base_url()?>index.php/feedback/load_feedback/<?php echo $dates[$date] ?>"><?php echo $semestre ?></a><br>
-			<?php }}?>
->>>>>>> 6368aba79a3e6bf7f1784fee66c02bd985d0234f
 		</div>
 		</div>
 	</div>
