1b32cd2e640b94ef401327daf59ab40537294171
danilosantoscjr <danilosantos@cjr.org.br>
2015-03-14 23:06:18 -0300

    Arrumando conflito /15

--- CodeIgniter_2.2.0/application/views/_inc/nav_bar.php
+++ CodeIgniter_2.2.0/application/views/_inc/nav_bar.php
@@ -1,13 +1,7 @@
 <div id="navbar">
             <a href="<?php echo base_url() ?>index.php/usuario/home"><li>Home</li></a>
             <a href="<?php echo base_url() ?>index.php/access/sign_out"><li>Sair</li></a>
-<<<<<<< HEAD
-=======
             <?php if($this->session->userdata('login_perfil') == 2){
             	echo '<a href="'.base_url().'index.php/admin/change_password'.'"><li>Senha</li></a>';
             	}  ?>
-        </ul>
-        </li>
-    </ul>
->>>>>>> 14231ee04e119cd9ba46cbf6ab3d3297744c06d8
 </div>
