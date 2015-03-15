52dad64aed78b39b979a14cdfbafb061d3088c77
igorsodre <igorcuei@yahoo.com.br>
2015-03-15 20:25:02 -0300

    conserto no alterar senha admin

--- CodeIgniter_2.2.0/application/views/admin/admin_change_password.php
+++ CodeIgniter_2.2.0/application/views/admin/admin_change_password.php
@@ -1,17 +1,29 @@
-<?php echo $this->load->view('_inc/header_thin') ?>
-<?php echo $this->load->view('_inc/nav_bar')?>
-<div class="header-text-thin-square-2 header-text-uppercase">
-	<p>Alterar Senha</p>
-</div>
-</div>
-</div>
-
-	<div id="content" class="content-large">
-        <?php echo form_open('admin/set_new_password') ?>
-        	<label>Insira a senha atual<input type="password" name="senha_old" value=""></label><br />
-        	<label>Insira a nova senha<input type="password" name="senha" value=""></label><br />
-        	<label>Confirme a nova senha<input type="password" name="novasenha" value=""></label><br />
-        	<input type="submit" value="Alterar">
-        <?php echo form_close() ?>
+<?php echo $this->load->view('_inc/header_boot') ?>
+                <div class="header-text-aside header-uppercase">
+                    <p>
+                    ALTERAR SENHA
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+<?php echo $this->load->view('_inc/nav_bar')?>
+    <div class="row">
+		<div class="col-md-6 col-md-offset-5 form-group form-column">
+			<?php echo form_open('admin/set_new_password') ?>
+	        	<label>Insira a senha atual<br />
+				<input type="password" class="form-control" name="senha_old" value=""></label><br />
+	        	<label>Insira a nova senha<br />
+				<input type="password" class="form-control" name="senha" value=""></label><br />
+	        	<label>Confirme a nova senha<br />
+				<input type="password" class="form-control" name="novasenha" value=""></label><br />
+		</div>
+    </div>
+	<div class="row">
+		<div class="col-md-12">
+			<input type="submit" class="btn btn-dark-accept center-block" value="">
+		<?php echo form_close() ?>
+		</div>
+	</div>
 <?php echo $this->load->view('_inc/footer') ?>
