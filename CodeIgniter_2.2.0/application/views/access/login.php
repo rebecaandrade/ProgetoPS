9cf0020cde958fafef6397571176c2da8905d464
danilosantoscjr <danilosantos@cjr.org.br>
2015-03-15 21:52:38 -0300

    Consertada página login

--- CodeIgniter_2.2.0/application/views/access/login.php
+++ CodeIgniter_2.2.0/application/views/access/login.php
@@ -10,14 +10,14 @@
         <div class="col-xs-12 col-md-6 col-md-offset-3">
             <div id="login-field" class="row center-block" >
                 <?php echo form_open('access/sign_in') ?>
-                    <div id="login-field-input" class="col-xs-9 col-md-9">
+                    <div id="login-field-input" class="col-xs-8 col-md-9">
                         <div class="form-group">
                             <label for="login-field-user"></label>
                             <input type="text" class="form-control" name="login" placeholder="USUÁRIO" id="login-field-user">
                             <label for="login-field-password"></label>
                             <input type="password" class="form-control" name="password" placeholder="SENHA" id="login-field-password">
                             <a href="<?php echo base_url(); ?>index.php/access/password_recovery">Esqueci minha senha</a>
-                            <input type="submit" class="btn btn-light-accept center-block" name="name" value="&nbsp" style="margin-top: 25px">
+                            <input type="submit" class="btn btn-light-accept center-block" name="name" value="&nbsp">
                         </div>
                     </div>
                     <a href="<?php echo base_url(); ?>index.php/usuario/create_user">
