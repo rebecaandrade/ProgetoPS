418832c5f1c1465f479ad8b787f3915d0adc89ad
danilosantoscjr <danilosantos@cjr.org.br>
2015-03-16 16:54:50 -0300

    commit

--- CodeIgniter_2.2.0/application/views/access/reset.php
+++ CodeIgniter_2.2.0/application/views/access/reset.php
@@ -1,33 +1,40 @@
 <?php echo $this->load->view('_inc/header_boot') ?>
-<div class="header-text-thin-square-2 header-text-uppercase">
-	<p>Recuperar Senha</p>
+                <div class="header-text-aside header-uppercase">
+                    <p>
+                    RECUPERAR SENHA
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
-</div>
-	<?php 
 
-		if(isset($encrypt) && $encrypt){ 
-			if(isset($time) && $time){
+    <div class="row">
+        <div id="reset-password" class="col-md-8 col-md-offset-2 form-group">
+        		<?php 
+		if(isset($encrypt)){ 
+			if(isset($encrypt)){
 			?>
-				<div id="content" class="content-large">
 			        <?php echo form_open('access/reset_password') ?>
-			        	<label>Insira a nova senha<input type="password" name="senha"></label><br />
-			        	<label>Confirme a nova senha<input type="password" name="novasenha"></label><br />
-			        	<input type="hidden" name="encrypt" value="<?php echo $encrypt?>">
-			        	<input type="submit" value="Alterar">
+			        	<label>Insira a nova senha<br /><input type="password" class="form-control center-block" name="senha"></label><br />
+			        	<label>Confirme a nova senha<br /><input type="password" class="form-control center-block" name="novasenha"></label><br />
+			        	<input type="hidden" class="form-control" name="encrypt" value="<?php echo $encrypt?>">
+			        	
+			        	<input type="submit" class="btn btn-dark-accept center-block" value="">
 			        <?php echo form_close() ?>
-			    </div>
 	    	<?php 
 	    	} else { ?>
-	    	<p>
+	    	<h3>
 	    		Link de confirmação expirou! Reenvie o e-mail para alterar a senha.
-	    	</p>
+	    	</h3>
 	    <?php 
 			} 
 		} else { ?>
-			<p>
+			<h3>
 				Acesse "Esqueci minha senha" para recuperar sua senha.
-			</p>
+			</h3>
 		<?php 
 		} ?>
+        </div>
+    </div>
 <?php echo $this->load->view('_inc/footer_boot') ?>
